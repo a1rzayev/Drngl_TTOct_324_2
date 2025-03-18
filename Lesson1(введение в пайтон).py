60affd12db0c1f7dbbe1cc19cf6dfd740eac2566
a1rzayev <asifrzayev2006@gmail.com>
2025-03-18 17:44:57 +0400

    Первый урок

---
+++ Lesson1(введение в пайтон).py
@@ -0,0 +1,15 @@
+"""функция print() вывод"""
+print("name:", "Iskender")
+print("age:", "13")
+
+"""переменные"""
+name = "Rafael"
+age = 13
+print("name:", name)
+print("age:", age)
+
+"""функция input() ввод"""
+name = input("Input name: ")
+age = int(input("Input age: "))
+print("name:", name)
+print("age:", age)
