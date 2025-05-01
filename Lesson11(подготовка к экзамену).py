fd0c91cf145d8890b3ba7a886fc405abae1dba18
a1rzayev <asifrzayev2006@gmail.com>
2025-05-01 17:49:05 +0400

    Подготовка к экзамену(разбор вопросов)

---
+++ Lesson11(подготовка к экзамену).py
@@ -0,0 +1,7 @@
+# вопрос Шамиля про валюту
+currency_list = ["dollar", "manat", "rubl"]
+currency = input("Enter currency:")
+if(currency in currency_list):
+    print("Shamil, ok")
+else: 
+    print("Shamil, dur get ishinle meshqul")
